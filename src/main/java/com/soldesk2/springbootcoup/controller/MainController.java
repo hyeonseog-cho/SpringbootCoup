bc3d5644389ee72d00ba72f8a6368e0f807fdc1c
JHS0425 <mad02972@gmail.com>
2023-03-28 00:17:03 +0900

    2312

--- src/main/java/com/soldesk2/springbootcoup/controller/MainController.java
+++ src/main/java/com/soldesk2/springbootcoup/controller/MainController.java
@@ -9,6 +9,6 @@ public class MainController {
     
     @GetMapping({"/","/index"})
     public String index() {
-        return "Java Spring boot BoardGame Coup";
+        return "2Team Java Spring boot BoardGame_Coup";
     }
 }
